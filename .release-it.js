f495eab32aa6a80de224cd89202b5d2af36c3358
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2021-10-06 23:19:06 +0300

    chore(deps): connect release system

---
+++ .release-it.js
@@ -0,0 +1,18 @@
+module.exports = {
+    git: {
+        commitMessage: 'v${version}',
+    },
+    npm: {
+        publish: false,
+    },
+    github: {
+        release: true,
+        releaseName: 'v${version}',
+    },
+    hooks: {
+        'after:bump': 'npx auto-changelog',
+    },
+    hooks: {
+        'after:bump': 'npx auto-changelog -p',
+    },
+};
